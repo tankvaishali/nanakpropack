673e22a2b19035ca7e08eaf353e5955e2009176a
tankvaishali <vaishalitank28603@gmail.com>
2025-04-07 09:33:57 +0530

    craete home folder

---
+++ src/HOC.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HOC() {
+    function NewComponent() {
+        return (
+            <div>
+              
+            </div>
+          );
+    }
+  return NewComponent
+}
+
+export default HOC;
