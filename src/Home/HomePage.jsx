355dfa512b09f3190246fa5908dc021699f2a423
tankvaishali <vaishalitank28603@gmail.com>
2025-04-09 12:41:28 +0530

    whychooseus add section

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -5,11 +5,13 @@ import HomeAboutSec from './HomeAboutSec';
 import IndustrySection from './IndustrySection';
 import HomeService from './HomeService';
 import HomeProduct from './HomeProduct';
+import Whychooseus from './Whychooseus';
 
 function HomePage() {
     return (
         <>
             <HomeAboutSec />
+            <Whychooseus/>
             <HomeProduct/>
             <HomeService />
             <IndustrySection />
